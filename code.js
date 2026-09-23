f4eacb7327e1ebd42e0506f6635f66863cca7c05
junicookie <juni.stenberg@outlook.com>
2026-09-23 12:51:26 +0200

    Uppgift 4

---
+++ code.js
@@ -0,0 +1,71 @@
+const input = document.querySelector('input');
+const addBtn = document.getElementById("addButton")
+const button = document.querySelector('button');
+const list = document.querySelector("ul")
+const completedTask = document.getElementById("taskCounter")
+
+
+let completedNum = 0;
+let tasks = [];
+
+
+
+function addTask() {
+//Check is value is empty and send alert
+if (input.value.trim() === "") {
+alert("Please write something in the text box.")
+return; 
+
+} else {
+// Create a list and span-element. Add value of input. 
+const addList = document.createElement("li")
+const addSpanElement = document.createElement("span");
+
+addSpanElement.innerText = input.value;
+addList.appendChild(addSpanElement);
+list.appendChild(addList);
+
+//Insert text to array and get position in array
+tasks.push(input.value);
+const taskIndex = tasks.length - 1;
+
+//Insert delete button and empty text box field.
+addDeleteBtn(addList, taskIndex);
+input.value = ""
+}}
+
+//Adding counter for completed tasks
+function counter(){
+ completedNum = list.querySelectorAll(".complete").length   
+ completedTask.innerText = (`${completedNum} tasks completed`)
+}
+
+//Create delete button
+function addDeleteBtn(taskElement, index) {
+const deleteBtn = document.createElement("button")
+deleteBtn.textContent = "🗑️";
+deleteBtn.id = "deleteBtn"
+
+//Remove task on button "click"
+deleteBtn.addEventListener("click", () => {
+    taskElement.remove();
+    tasks.splice(index, 1)
+});
+
+taskElement.appendChild(deleteBtn);
+}
+
+//Toggle between finished/unfinished on "click" 
+list.addEventListener("click", (text) => {
+if(text.target.tagName === "SPAN") {
+text.target.classList.toggle("complete")
+counter();}
+})
+
+
+//Create list when "Add to list" is clicked. 
+button.addEventListener("click", addTask)
+
+
+
+
